ee27ca7a884ef982a75ba86137e7971730b35e1b
james-muriithi <muriithijames556@gmail.com>
2022-05-12 01:33:56 +0300

    added placeholder details in repo card

--- src/components/RepoCard.vue
+++ src/components/RepoCard.vue
@@ -1,12 +1,31 @@
 <template>
-    <div class="repo-card">
-        
+  <div class="repo-card bg-white p-3 p-md-4">
+    <a href="#" target="_blank" rel="noopener noreferrer">
+      developit/microbundle
+    </a>
+    <p class="description mt-3">Zero-configuration bundler for tiny modules</p>
+    <div class="row">
+      <div class="col">
+        <p class="text-muted mb-0">Created on 1st may 2022</p>
+      </div>
+      <div class="col d-flex">
+        <span class="d-flex justify-content-center ms-auto">
+          <img src="@/assets/images/star.svg" alt="stars" height="22" />
+          <span class="ps-1">1000</span>
+        </span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "RepoCard",
-
-}
+  name: "RepoCard",
+};
 </script>
+
+<style lang="scss" scoped>
+.repo-card {
+  box-shadow: 0px 10px 35px -3px rgba(0, 0, 0, 0.05);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="home">
+  <div class="home mt-5">
+    <h3 class="fw-bold mb-2">Most stars: '{{ language }}'</h3>
+    <p>
+      <i> Repos created since 1st may 2022 </i>
+    </p>
+
+    <div class="pt-4">
+      <RepoCard class="mb-4" />
+    </div>
   </div>
 </template>
 
 <script>
-
+import RepoCard from '@/components/RepoCard.vue';
 export default {
-  name: 'HomeView',
-}
+  components: { RepoCard },
+  name: "HomeView",
+  computed: {
+    language() {
+      return "javascript";
+    },
+  },
+};
 </script>
